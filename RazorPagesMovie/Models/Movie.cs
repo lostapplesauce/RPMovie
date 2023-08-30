7e69e10caba5d9c30c30846a32ecb1fb46469807
lostapplesauce <48301604+lostapplesauce@users.noreply.github.com>
2023-08-30 18:21:07 +0100

    first commit

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations; //Set of built-in validation attributes that are applied declaratively to a class or property
+                                             //Formatting attributes like [DataType] that help with fomratting and don't provide any validation
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        
+        [StringLength(60, MinimumLength = 2)]
+        [Required]
+        public string? Title { get; set; }
+
+        [Display(Name ="Release Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)] //ApplyFormatInEditMode makes it so when user edits it'll be in this format
+                                                                                           //Good to leave this false for some instances such as price
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Range(0, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName= "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9]")]
+        [Required]
+        [StringLength(30)]
+        public string? Genre { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        public string Rating { get; set; } = string.Empty;
+    }
+}
---
+++ RazorPagesMovie/Models/SeedData.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+
+namespace RazorPagesMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPagesMovieContext>>()))
+            {
+                if(context == null || context.Movie == null)
+                {
+                    throw new ArgumentNullException("Null RazorPagesMovieContext");
+                }
+
+                //Look for any movies
+                //Previously I had a movie in the database...
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+
+                //...So these movies weren't added.
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                        Rating = "R"
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M,
+                        Rating = "PG"
+                    },
+                    new Movie
+                    {
+                        Title = "GhostBusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M,
+                        Rating = "PG-13"
+                    },  
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M,
+                        Rating = "PG"
+                    }
+                    );
+                context.SaveChanges();
+            }
+            //Add-Migration in the Nuget console Compare the Movie model with the Movie database schema
+            // Create code to migrate teh databse schema to the new model
+
+            //Update-Database tells the framework to apply the schema changes to the database and to preserve existing data.
+        }
+    }
+}
